8ee60c0df340fe91cad2a69649d7a2bb4ab09c68
gyzong1 <40457486+gyzong1@users.noreply.github.com>
2019-05-26 22:16:08 +0800

    Update npm-example-pipeline.groovy

--- scripts/npm-example-pipeline.groovy
+++ scripts/npm-example-pipeline.groovy
@@ -8,29 +8,32 @@ node {
     def buildInfo
 
     stage ('Clone') {
-        git url: 'https://github.com/JFrog/project-examples.git'
+        git url: 'https://github.com/gyzong1/pipeline-example.git'
     }
 
     stage ('Artifactory configuration') {
-        rtNpm.deployer repo: 'npm-local', server: server
-        rtNpm.resolver repo: 'npm-remote', server: server
+        rtNpm.deployer repo: 'npm-dev-local', server: server
+        rtNpm.resolver repo: 'npm-virtual', server: server
         rtNpm.tool = 'nodejs' // Tool name from Jenkins configuration
         buildInfo = Artifactory.newBuildInfo()
     }
 
 
     stage ('Install npm') {
-        rtNpm.install buildInfo: buildInfo, path: 'npm-example'
+        rtNpm.install buildInfo: buildInfo, path: 'project-examples/npm-example'
     }
 
     stage ('Publish npm') {
-        rtNpm.publish buildInfo: buildInfo, path: 'npm-example'
+        rtNpm.publish buildInfo: buildInfo, path: 'project-examplts/npm-example'
     }
 
     stage ('Publish build info') {
         server.publishBuildInfo buildInfo
     }
     
-    sh "curl -uadmin:admin -X POST 'http://192.168.230.135:8081/artifactory/api/copy/npm-local/npm-example/-/npm-example-0.0.3.tgz?to=/npm-local/npm-example/-/npm-example-0.0.3.${env.BUILD_NUMBER}.tgz'"
-
+    /*
+    stage ('Copy and rename') {
+        sh "curl -uadmin:password -X POST 'http://192.168.230.155:8081/artifactory/api/copy/npm-dev-local/npm-example/-/npm-example-0.0.3.tgz?to=/npm-local/npm-example/-/npm-example-0.0.3.${env.BUILD_NUMBER}.tgz'"
+    }
+    */
 }
