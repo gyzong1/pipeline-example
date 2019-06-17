c97882b93370e6bf378d92956519875b756e3777
gyzong1 <40457486+gyzong1@users.noreply.github.com>
2019-06-17 15:32:12 +0800

    Create go-example-cli.groovy

---
+++ scripts/go-example-cli.groovy
@@ -0,0 +1,40 @@
+node {
+    
+    env.NODE_HOME=tool name: 'go', type: 'go'
+    env.PATH="${env.GOROOT}/bin:${env.PATH}"
+    
+    stage('Prepare') {
+        sh 'jfrog rt c art1 --url=http://192.168.230.155:8081/artifactory --user=admin --password=password'        // 此处使用域名不好使，具体原因待查
+        sh 'jfrog rt use art1'
+    }
+    stage('SCM') {
+        // cleanWs()
+        sh 'ls'
+        git([url: 'https://github.com/gyzong1/pipeline-example.git', branch: 'master'])
+    }
+    
+    stage('Build') {
+        dir('project-examples/golang-example') {
+          sh "jfrog rt go build go-virtual --build-name=${env.JOB_NAME} --build-number=${env.BUILD_NUMBER}"
+        }
+    }
+    
+    stage('Publish packages') {
+        dir('project-examples/golang-example') {
+          sh "jfrog rt gp go-virtual v1.0.0 --build-name=${env.JOB_NAME} --build-number=${env.BUILD_NUMBER}"
+        }
+    }
+    
+    stage('Collect environment variables') {
+        dir('project-examples/golang-example') {
+          sh "jfrog rt bce ${env.JOB_NAME} ${env.BUILD_NUMBER}"
+        }
+    }
+    
+    stage('Publish the build info') {
+        dir('project-examples/golang-example') {
+          sh "jfrog rt bp ${env.JOB_NAME} ${env.BUILD_NUMBER}"
+        }
+    }
+    
+}
