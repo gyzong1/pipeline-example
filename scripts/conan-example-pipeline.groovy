87b5cb07ef337914cc0a45ff22734e1c07bf5530
gyzong1 <40457486+gyzong1@users.noreply.github.com>
2019-06-18 11:13:44 +0800

    Update conan-example-pipeline.groovy

--- scripts/conan-example-pipeline.groovy
+++ scripts/conan-example-pipeline.groovy
@@ -1,32 +1,42 @@
- node {
-        sh "conan config set general.revisions_enabled=1"
-        // Clone the code from github:
-        git url :'https://github.com/memsharded/example-poco-timer.git'
+node {
+        
+        stage("Scm"){
+            // Clone the code from github:
+            git url :'https://github.com/memsharded/example-poco-timer.git'
+        }
+ 
+        stage("Conan Config"){
+             // Obtain an Artifactory server instance, defined in Jenkins --> Manage:
+            def server = Artifactory.server 'art1'
+    
+            // Create a local build-info instance:
+            def buildInfo = Artifactory.newBuildInfo()
+            buildInfo.name = "conan-example-pipeline"
+    
+            // Create a conan client instance:
+            def conanClient = Artifactory.newConanClient()
+    
+            // Add a new repository named 'conan-local' to the conan client.
+            // The 'remote.add' method returns a 'serverName' string, which is used later in the script:
+            String serverName = conanClient.remote.add server: server, repo: "conan-local"
+        }
+ 
+        stage("Build"){
+            // Run a conan build. The 'buildInfo' instance is passed as an argument to the 'run' method:
+            conanClient.run(command: "install . --build missing", buildInfo: buildInfo)
+        }
 
-         // Obtain an Artifactory server instance, defined in Jenkins --> Manage:
-        def server = Artifactory.server 'art1'
+        stage("Upload"){
+            // Create an upload command. The 'serverName' string is used as a conan 'remote', so that
+            // the artifacts are uploaded into it:
+            String command = "upload * --all -r ${serverName} --confirm"
+        }  
 
-        // Create a local build-info instance:
-        def buildInfo = Artifactory.newBuildInfo()
-        buildInfo.name = "conan-example-pipeline"
-
-        // Create a conan client instance:
-        def conanClient = Artifactory.newConanClient()
-
-        // Add a new repository named 'conan-local' to the conan client.
-        // The 'remote.add' method returns a 'serverName' string, which is used later in the script:
-        String serverName = conanClient.remote.add server: server, repo: "conan-virtual"
-
-        // Run a conan build. The 'buildInfo' instance is passed as an argument to the 'run' method:
-        conanClient.run(command: "install . --build missing", buildInfo: buildInfo)
-
-        // Create an upload command. The 'serverName' string is used as a conan 'remote', so that
-        // the artifacts are uploaded into it:
-        String command = "upload * --all -r ${serverName} --confirm"
-
-        // Run the upload command, with the same build-info instance as an argument:
-        conanClient.run(command: command, buildInfo: buildInfo)
-
-         // Publish the build-info to Artifactory:
-        server.publishBuildInfo buildInfo
+        stage("Publish"){
+            // Run the upload command, with the same build-info instance as an argument:
+            conanClient.run(command: command, buildInfo: buildInfo)
+    
+             // Publish the build-info to Artifactory:
+            server.publishBuildInfo buildInfo
+        }
 }
