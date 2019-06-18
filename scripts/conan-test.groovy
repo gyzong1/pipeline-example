8c65a6cd9a353f914dc3adecb528f319943bea5f
gyzong1 <40457486+gyzong1@users.noreply.github.com>
2019-06-18 13:00:21 +0800

    Update conan-test.groovy

--- scripts/conan-test.groovy
+++ scripts/conan-test.groovy
@@ -7,33 +7,7 @@ node {
             //git url : 'https://github.com/lasote/conan-goserver-example'
         }
         stage("build&push"){
-             // Obtain an Artifactory server instance, defined in Jenkins --> Manage:
-            def server = Artifactory.server 'art1'
-    
-            // Create a local build-info instance:
-            def buildInfo = Artifactory.newBuildInfo()
-            buildInfo.name = "Conan-pipeline"
-    
-            // Create a conan client instance:
-            def conanClient = Artifactory.newConanClient()
-    
-            // Add a new repository named 'conan-local' to the conan client.
-            // The 'remote.add' method returns a 'serverName' string, which is used later in the script:
-            String serverName = conanClient.remote.add server: server, repo: "conan-local"
-    
-            // Run a conan build. The 'buildInfo' instance is passed as an argument to the 'run' method:
-            conanClient.run(command: "install . --build missing", buildInfo: buildInfo)
-    
-            // Create an upload command. The 'serverName' string is used as a conan 'remote', so that
-            // the artifacts are uploaded into it:
-            String command = "upload * --all -r ${serverName} --confirm"
-    
-            // Run the upload command, with the same build-info instance as an argument:
-            conanClient.run(command: command, buildInfo: buildInfo)
-    
-             // Publish the build-info to Artifactory:
-            server.publishBuildInfo buildInfo
-        }
-
-        
+            sh "conan install . --build missing"
+            sh "conan upload * --all -r conan-local --confirm"
+        }     
 }
