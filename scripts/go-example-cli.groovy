0a4e218ece8fad00e70d8a853bbbd7bb3146aaaf
gyzong1 <40457486+gyzong1@users.noreply.github.com>
2019-10-25 16:22:46 +0800

    Update go-example-cli.groovy

--- scripts/go-example-cli.groovy
+++ scripts/go-example-cli.groovy
@@ -22,6 +22,8 @@ node {
     stage('Publish packages') {
         dir('project-examples/golang-example/hello') {
           sh "jfrog rt gp go-virtual v1.0.0 --deps=ALL --build-name=${env.JOB_NAME} --build-number=${env.BUILD_NUMBER}"
+            
+          sh "jfrog rt grp go-virtual"
         }
     }
 
