0a64d49f20a0b7b144ba21ea3ef217dd594a8a1f
gyzong1 <40457486+gyzong1@users.noreply.github.com>
2019-11-19 11:32:07 +0800

    Update helm-demo-master.groovy

--- scripts/helm-demo-master.groovy
+++ scripts/helm-demo-master.groovy
@@ -16,7 +16,7 @@ node {
         // Specific target repo
         rtMaven.deployer releaseRepo: 'libs-release-local', snapshotRepo: 'libs-snapshot-local', server: artiServer
         try {
-            sh 'helm delete jfrog-helm-demo --purge'
+            sh 'h'
             sh 'sleep 5'
         } catch(Exception e) {
             println('remove resources in kubernetes failed, please check the log.')
@@ -40,13 +40,15 @@ node {
     }
     stage('Image') {
         // Build docker image
-        tagName = 'ha.example.com/docker-virtual/jfrog-cloud-demo:latest'
+        tagName = '192.168.230.155/docker-virtual/jfrog-cloud-demo:latest'
         docker.build(tagName)
-        sh 'docker login ha.example.com -u admin -p AKCp5ccv3oMbQuovKWLzCdRW2RnZW9Qb4agjxVA931J9SsJwwkEuAe1yknQtMBegJvDq8RSr8'
-        sh 'docker push ha.example.com/docker-virtual/jfrog-cloud-demo:latest'
+        sh 'docker login 192.168.230.155 -u admin -p AKCp5ccv3oMbQuovKWLzCdRW2RnZW9Qb4agjxVA931J9SsJwwkEuAe1yknQtMBegJvDq8RSr8'
+        sh 'docker push 192.168.230.155/docker-virtual/jfrog-cloud-demo:latest'
         // sh 'docker rmi docker-local.artifactory.cloud.demo/jfrog-cloud-demo:latest'
         // sh 'docker logout docker-local.artifactory.cloud.demo'
     }
+
+
     stage('Test') {
         // Smoke test
         docker.image(tagName).withRun('-p 8181:8080') { c->
@@ -54,19 +56,28 @@ node {
             //sh 'curl "http://127.0.0.1:8181"'
         }
     }
+
     stage('Package Helm Chart'){
         // Package code to Helm Chart
         sh 'helm package jfrog-cloud-chart --debug'
     }
+
+    stage('Add Helm repo'){
+        // Package code to Helm Chart
+        sh 'helm repo add helm-virtual http://192.168.230.155/artifactory/helm-virtual --username admin --password AKCp5ccv3oMbQuovKWLzCdRW2RnZW9Qb4agjxVA931J9SsJwwkEuAe1yknQtMBegJvDq8RSr8'
+    }   
+
     stage('Upload Chart'){
         // Upload Chart to artifactory helm repository
 	// sh 'helm repo add helm-virtual http://ha.example.com/artifactory/helm-virtual --username admin --password AKCp5ccv3oMbQuovKWLzCdRW2RnZW9Qb4agjxVA931J9SsJwwkEuAe1yknQtMBegJvDq8RSr8'
-        sh 'curl -u admin:password -X PUT "http://ha.example.com/helm-virtual/jfrog-cloud-chart-0.1.0.tgz" -T jfrog-cloud-chart-0.1.0.tgz'
+        sh 'curl -u admin:password -X PUT "http://192.168.230.155/helm-virtual/jfrog-cloud-chart-0.1.0.tgz" -T jfrog-cloud-chart-0.1.0.tgz'
     }
+
     stage('Deploy Helm Chart'){
         // Deploy to kubernetes via helm client
         sh 'helm repo update'
         sh 'helm install helm-virtual/jfrog-cloud-chart --name jfrog-helm-demo'
     }
+
     }
 }
