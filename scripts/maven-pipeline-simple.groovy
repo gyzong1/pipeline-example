806b3b34fb947bed9fe30b75913133b923da660b
gyzong1 <40457486+gyzong1@users.noreply.github.com>
2019-11-24 12:12:39 +0800

    Update maven-pipeline-simple.groovy

--- scripts/maven-pipeline-simple.groovy
+++ scripts/maven-pipeline-simple.groovy
@@ -27,8 +27,8 @@ node {
 
     stage ('Artifactory configuration') {
         rtMaven.tool = 'maven' // Tool name from Jenkins configuration
-        rtMaven.deployer releaseRepo: 'maven-virtual', snapshotRepo: 'maven-virtual', server: artServer
-        rtMaven.resolver releaseRepo: 'maven-test-local', snapshotRepo: 'maven-test-local', server: artServer
+        rtMaven.resolver releaseRepo: 'maven-virtual', snapshotRepo: 'maven-virtual', server: artServer
+        rtMaven.deployer releaseRepo: 'maven-test-local', snapshotRepo: 'maven-test-local', server: artServer
     }
 
     stage ('Exec Maven') {
