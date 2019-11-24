b77f19a0a7add34ccd53f64172772de79f1831db
gyzong1 <40457486+gyzong1@users.noreply.github.com>
2019-11-25 00:30:08 +0800

    Update maven-pipeline-simple.groovy

--- scripts/maven-pipeline-simple.groovy
+++ scripts/maven-pipeline-simple.groovy
@@ -27,14 +27,37 @@ node {
 
     stage ('Artifactory configuration') {
         rtMaven.tool = 'maven' // Tool name from Jenkins configuration
+        // 或 env.MAVEN_HOME = '/tools/apache-maven-3.3.9'
+        // 指定jdk版本
+        // env.JAVA_HOME = 'full/path/to/JDK'
         rtMaven.resolver releaseRepo: 'maven-virtual', snapshotRepo: 'maven-virtual', server: artServer
         rtMaven.deployer releaseRepo: 'maven-test-local', snapshotRepo: 'maven-test-local', server: artServer
-        // 只包含某部分包
+      
+        // 只包含某部分包，或排除某部分
         // rtMaven.deployer.artifactDeploymentPatterns.addInclude("*multi3*")
+        // rtMaven.deployer.artifactDeploymentPatterns.addExclude("*.zip")
+        // rtMaven.deployer.artifactDeploymentPatterns.addInclude("frog*").addExclude("*.zip")
+      
         // 添加属性
         rtMaven.deployer.addProperty("status", "in-qa").addProperty("compatibility", "1", "2", "3")
+      
         // 禁用部署工件
-        rtMaven.deployer.deployArtifacts = false
+        // rtMaven.deployer.deployArtifacts = false
+        // 稍后发布工件
+        // rtMaven.deployer.deployArtifacts buildInfo
+      
+        
+        if (buildInfo.getDependencies().size() > 0) {
+            def localPath = buildInfo.getDependencies()[0].getLocalPath()
+            def remotePath = buildInfo.getDependencies()[0].getRemotePath()
+            def md5 = buildInfo.getDependencies()[0].getMd5()
+            def sha1 = buildInfo.getDependencies()[0].getSha1()
+          
+            echo "$localPath"
+            echo "$remotePath"
+            echo "$md5"
+            echo "sha1"
+        }
       
     }
 
