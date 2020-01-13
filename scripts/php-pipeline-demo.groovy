e1d4abad6bc6b8b05d5fe8d5a3eb279c8d8a93df
gyzong1 <40457486+gyzong1@users.noreply.github.com>
2020-01-14 07:18:14 +0800

    Update php-pipeline-demo.groovy

--- scripts/php-pipeline-demo.groovy
+++ scripts/php-pipeline-demo.groovy
@@ -1,14 +1,27 @@
 node {
-    stage('SCM') {
-
-    }
 
     stage('SCM') {
-
+        // cleanWs()
+        sh 'ls'
+        git([url: 'https://github.com/gyzong1/pipeline-example.git', branch: 'master'])
     }
-
-    stage('SCM') {
-
+    
+    stage('Build') {
+        dir('project-examples/php-example') {
+          sh "php /usr/local/bin/composer init"
+        }
     }
-
+    
+    stage('Package') {
+        dir('project-examples/php-example') {
+            sh "cd .. && tar -zcvf php-demo-${env.BUILD_NUMBER}.tar.gz ${env.JOB_NAME}"
+        }
+    }
+    
+    stage('Upload') {
+        dir('project-examples/php-example') {
+            sh "cd .. && curl -uadmin:password -X PUT http://192.168.230.155:8081/artifactory/composer-dev-local/${env.BUILD_NUMBER}/php-demo-${env.BUILD_NUMBER}.tar.gz -T php-demo-${env.BUILD_NUMBER}.tar.gz"
+        }
+    }
+    
 }
