0a040e4acf6e6921fee85990216068626f045af9
gyzong1 <40457486+gyzong1@users.noreply.github.com>
2020-01-14 10:45:52 +0800

    Update conan-test.groovy

--- scripts/conan-test.groovy
+++ scripts/conan-test.groovy
@@ -9,7 +9,7 @@ node {
         
         stage("config"){
             //sh "conan remote add conan http://192.168.230.155/artifactory/api/conan/conan-local"
-            sh "conan user -p AKCp5ccv3oMbQuovKWLzCdRW2RnZW9Qb4agjxVA931J9SsJwwkEuAe1yknQtMBegJvDq8RSr8 -r conan admin"
+            sh "conan user -p AKCp5ccv3oMbQuovKWLzCdRW2RnZW9Qb4agjxVA931J9SsJwwkEuAe1yknQtMBegJvDq8RSr8 -r test admin"
         }  
         
         stage("build&push"){
