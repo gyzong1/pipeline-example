f3038f74dcd5aeb337460aac0a33aed83e5409be
gyzong1 <40457486+gyzong1@users.noreply.github.com>
2020-01-14 10:50:58 +0800

    Update conan-test.groovy

--- scripts/conan-test.groovy
+++ scripts/conan-test.groovy
@@ -14,7 +14,7 @@ node {
         }  
         
         stage("build&push"){
-            sh "conan install . -r --build missing"
+            sh "conan install . -r test --build missing"
             sh "conan upload * --all -r test --confirm"
         }     
 }
