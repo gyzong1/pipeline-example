02b23505d0bd7ef920c654cee42cf2f36c7da3b7
gyzong1 <40457486+gyzong1@users.noreply.github.com>
2020-01-16 11:43:20 +0800

    Update gradle-example-pipeline.groovy

--- scripts/gradle-example-pipeline.groovy
+++ scripts/gradle-example-pipeline.groovy
@@ -9,7 +9,7 @@ node {
 
     stage ('Artifactory configuration') {
         rtGradle.tool = 'gradle' // Tool name from Jenkins configuration
-        rtGradle.useWrapper = false
+        rtGradle.useWrapper = true
         rtGradle.deployer repo: 'gradle-virtual', server: server
         rtGradle.resolver repo: 'gradle-virtual', server: server
     }
